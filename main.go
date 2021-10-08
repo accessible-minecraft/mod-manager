a8e2df4859981414985095ae568209c59d53d60c
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-08 15:49:43 +0530

    wow this is easy

--- main.go
+++ main.go
@@ -1,35 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
 
 func main() {
-
-	fmt.Println("Prime Counter using Recursion")
-	fmt.Println("Enter range:\t")
-	var r int
-	fmt.Scanln(&r)
-	count := prime(2, r)
-
-	fmt.Println("Total prime numbers till ", r, " are ", count)
-
-}
-
-func prime(current int, end int) int {
-	if current > end {
-		return 0
-	} else {
-		if isPrime(current) {
-			return 1 + prime(current+1, end)
-		}
-		return prime(current+1, end)
-	}
-}
-
-func isPrime(number int) bool {
-	for i := 2; i <= number/2; i++ {
-		if number%i == 0 {
-			return false
-		}
-	}
-	return true
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
 }
