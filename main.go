74dee58487b3c0c9fa20a1f738ebb4770747d4f8
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-08 15:42:58 +0530

    prime counter using recursion

--- main.go
+++ main.go
@@ -4,15 +4,32 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("multi return")
+	fmt.Println("Prime Counter using Recursion")
+	fmt.Println("Enter range:\t")
+	var r int
+	fmt.Scanln(&r)
+	count := prime(2, r)
 
-	i, result := multiReturn()
-
-	fmt.Println(i)
-	fmt.Println(result)
+	fmt.Println("Total prime numbers till ", r, " are ", count)
 
 }
 
-func multiReturn() (int, string) {
-	return 1, "shoaib"
+func prime(current int, end int) int {
+	if current > end {
+		return 0
+	} else {
+		if isPrime(current) {
+			return 1 + prime(current+1, end)
+		}
+		return prime(current+1, end)
+	}
+}
+
+func isPrime(number int) bool {
+	for i := 2; i <= number/2; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
 }
