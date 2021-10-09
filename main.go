9dfe332a0b9bb3b877f7f911b9f19b11e95264be
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-09 23:45:52 +0530

    interface

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Structs")
 
-	e1 := &Employee{
-		Name:    "Shoaib",
-		SurName: "Khan",
-		age:     18,
-	}
-
-	fmt.Println("Name:\t", (*e1).Name)
-	fmt.Println("Sur-Name:\t", (*e1).SurName)
-	fmt.Println("Age:\t", (*e1).age)
-	(*e1).AddAge()
-	fmt.Println("Age:\t", (*e1).age)
-
+	e1 := *NewEmployee1("shoaib", "khan")
+	e2 := *NewEmployee2("yusuf", "khan")
+	fmt.Println(e1)
+	ChangeAge(e1)
+	fmt.Println(e2)
+	ChangeAge(e2)
 }
 
-type Employee struct {
+func ChangeAge(obj AgeInterface) {
+	fmt.Println(obj.Age())
+}
+
+type AgeInterface interface {
+	Age() int
+}
+
+func NewEmployee1(name string, surName string) *Employee1 {
+	e := Employee1{
+		Name:    name,
+		SurName: surName,
+		age:     15,
+	}
+	return &e
+}
+
+func NewEmployee2(name string, surName string) *Employee2 {
+	e := Employee2{
+		Name:    name,
+		SurName: surName,
+		age:     18,
+	}
+	return &e
+}
+
+type Employee1 struct {
 	Name, SurName string
 	age           int
 }
 
-func (obj *Employee) AddAge() {
-	obj.age += 5
+func (obj Employee1) Age() int {
+	return obj.age + 5
+}
+
+type Employee2 struct {
+	Name, SurName string
+	age           int
+}
+
+func (obj Employee2) Age() int {
+	return obj.age - 5
 }
