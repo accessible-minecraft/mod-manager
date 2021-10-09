eaee25a323a5c414b18439137dfffb2d5e74faae
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-09 23:22:39 +0530

    Structs and methods

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
-import (
-	"io"
-	"net/http"
-)
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+	fmt.Println("Structs")
+
+	e1 := &Employee{
+		Name:    "Shoaib",
+		SurName: "Khan",
+		age:     18,
+	}
+
+	fmt.Println("Name:\t", (*e1).Name)
+	fmt.Println("Sur-Name:\t", (*e1).SurName)
+	fmt.Println("Age:\t", (*e1).age)
+	(*e1).AddAge()
+	fmt.Println("Age:\t", (*e1).age)
+
+}
+
+type Employee struct {
+	Name, SurName string
+	age           int
+}
+
+func (obj *Employee) AddAge() {
+	obj.age += 5
 }
