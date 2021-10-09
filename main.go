4a54bfeabd7209954709731e8c9e3e0b046295f4
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-10 00:00:29 +0530

    Reading JSON

--- main.go
+++ main.go
@@ -1,60 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Structs")
+	fmt.Println("JSON")
 
-	e1 := *NewEmployee1("shoaib", "khan")
-	e2 := *NewEmployee2("yusuf", "khan")
-	fmt.Println(e1)
-	ChangeAge(e1)
-	fmt.Println(e2)
-	ChangeAge(e2)
-}
-
-func ChangeAge(obj AgeInterface) {
-	fmt.Println(obj.Age())
-}
-
-type AgeInterface interface {
-	Age() int
-}
-
-func NewEmployee1(name string, surName string) *Employee1 {
-	e := Employee1{
-		Name:    name,
-		SurName: surName,
-		age:     15,
+	file, err := os.Open("/home/shoaib/.minecraft/home/Fabric-1.17/config/easylife/config.json")
+	if err != nil {
+		panic(err)
 	}
-	return &e
-}
 
-func NewEmployee2(name string, surName string) *Employee2 {
-	e := Employee2{
-		Name:    name,
-		SurName: surName,
-		age:     18,
+	defer file.Close()
+
+	byteCode, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
 	}
-	return &e
-}
 
-type Employee1 struct {
-	Name, SurName string
-	age           int
-}
-
-func (obj Employee1) Age() int {
-	return obj.age + 5
-}
-
-type Employee2 struct {
-	Name, SurName string
-	age           int
-}
-
-func (obj Employee2) Age() int {
-	return obj.age - 5
+	var result map[string]interface{}
+	json.Unmarshal(byteCode, &result)
+	fmt.Println(result["player_warnings_timeout"])
 }
