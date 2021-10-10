6e2e544c4fef9be95f8db5342ee0783de66ea706
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-10 18:46:39 +0530

    Teseted jar file downloading

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
@@ -12,7 +10,7 @@ import (
 func main() {
 	fmt.Println("JSON")
 
-	url := "https://raw.githubusercontent.com/accessible-minecraft/accessibility-plus-extended/master/Accessibility%20Plus%20Extended/src/main/resources/fabric.mod.json"
+	url := "https://github.com/accessible-minecraft/accessibility-plus-extended/raw/master/accessibility-plus-extended-1.16-curseforge-v1.6.4.jar"
 
 	// Get the data
 	resp, err := http.Get(url)
@@ -20,31 +18,12 @@ func main() {
 	defer resp.Body.Close()
 
 	// Create the file
-	out, err := os.Create("./temp.json")
+	out, err := os.Create("./ape.jar")
 	check(err)
 	defer out.Close()
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
-	check(err)
-
-	file, err := os.Open("./temp.json")
-	check(err)
-	defer file.Close()
-
-	byteCode, _ := ioutil.ReadAll(file)
-
-	var result map[string]interface{}
-	json.Unmarshal(byteCode, &result)
-
-	result["schemaVersion"] = 2.0
-
-	fmt.Println(result["schemaVersion"])
-
-	// Write the body to file
-	b, _ := json.Marshal(&result)
-	fileStat, _ := os.Lstat("./temp.json")
-	err = ioutil.WriteFile("./temp.json", b, fileStat.Mode().Perm())
 	check(err)
 
 }
