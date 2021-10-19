d2a82c2d95ad43f0a5f87d97e76c77bfb6c28a40
Shoaib Khan <shoaib.khan20@outlook.com>
2021-10-19 10:15:54 +0530

    Removed fyne

--- go.mod
+++ go.mod
@@ -1,27 +1,3 @@
 module github.com/accessible-minecraft/mod-manager
 
 go 1.17
-
-require (
-	fyne.io/fyne/v2 v2.1.0
-	github.com/stretchr/testify v1.7.0
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/go-gl/gl v0.0.0-20210905235341-f7a045908259 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
-	github.com/godbus/dbus/v5 v5.0.5 // indirect
-	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/srwiley/oksvg v0.0.0-20210519022825-9fc0c575d5fe // indirect
-	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
-	github.com/yuin/goldmark v1.4.1 // indirect
-	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
-	golang.org/x/net v0.0.0-20211015174653-db2dff38ab41 // indirect
-	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"runtime"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-
-	"github.com/accessible-minecraft/mod-manager/gui"
 )
 
 const (
@@ -17,73 +11,34 @@ const (
 )
 
 type mainApplication struct {
-	app            fyne.App
-	window         fyne.Window
-	currentContent *fyne.Container
-}
-
-func (m *mainApplication) chooseDirectory(w fyne.Window) {
-	dialog.ShowFolderOpen(func(dir fyne.ListableURI, err error) {
-		if err != nil {
-			dialog.ShowError(err, w)
-			return
-		}
-		if dir != nil {
-			save_dir := dir.Path() // here value of save_dir shall be updated!
-			isValid, err := checkIfValidOrNot(save_dir)
-
-			if isValid && err == nil {
-				m.app.Preferences().SetString(DefaultProfileKey, save_dir)
-			} else {
-				dialog.ShowInformation("Error", "The selected directory is not a valid minecraft directory", m.window)
-			}
-		}
-	}, w)
 }
 
 func main() {
-
-	mainApplication := mainApplication{app: app.NewWithID("com.shoaibkhan.modmanager")}
-
-	mainApplication.setContentToHome()
+	mainApplication := &mainApplication{}
 
 	mainApplication.setMinecraftDirectory()
 
-	defer mainApplication.window.ShowAndRun()
-
-}
-
-func (m *mainApplication) setContentToHome() {
-	m.window = m.app.NewWindow("Mod Manager")
-	m.window.SetTitle("Mod Manager")
-
-	profileButton := gui.GetProfileButton(m.app, "Selected Profile")
-	profileButton.Resize(fyne.NewSize(200, 30))
-
-	content := container.New(&gui.CustomLayout{}, profileButton)
-
-	m.currentContent = content
-
-	m.window.SetContent(content)
+	fmt.Println("Removed fyne")
 }
 
 func (m *mainApplication) setMinecraftDirectory() {
-	if m.app.Preferences().String(DefaultProfileKey) != "" {
-		isValid, err := checkIfValidOrNot(m.app.Preferences().String(DefaultProfileKey))
-		if !isValid && err != nil {
-			m.chooseDirectory(m.window)
-		}
-		return
-	}
+	// FIXME Redo this for go-a
+	// if m.app.Preferences().String(DefaultProfileKey) != "" {
+	// 	isValid, err := checkIfValidOrNot(m.app.Preferences().String(DefaultProfileKey))
+	// 	if !isValid && err != nil {
+	// 		m.chooseDirectory(m.window)
+	// 	}
+	// 	return
+	// }
 
 	minecraftDirectory := getMinecraftDirectory()
 
 	isValid, err := checkIfValidOrNot(minecraftDirectory)
 
 	if isValid && err == nil {
-		m.app.Preferences().SetString(DefaultProfileKey, minecraftDirectory)
+		// TODO save directory here
 	} else {
-		m.chooseDirectory(m.window)
+		// TODO Ask the user to enter directory here
 	}
 }
 
