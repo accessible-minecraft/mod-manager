790d4a6f12d0d3bf4a5e8f3f28b9c96330fd460a
Shoaib Khan <shoaib.khan20@outlook.com>
2021-11-03 12:52:08 +0530

    Added get supported mods list method successfully

--- mod-manager/src/main/java/com/shoaibkhan/modmanager/mods/GetSupportedModsList.java
+++ mod-manager/src/main/java/com/shoaibkhan/modmanager/mods/GetSupportedModsList.java
@@ -1,29 +1,50 @@
 package com.shoaibkhan.modmanager.mods;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.commons.io.FileUtils;
 
 public class GetSupportedModsList {
-    private static final String FILE_NAME = "mods.json";
-    String FILE_URL = "https://raw.githubusercontent.com/accessible-minecraft/mod-manager-configs/main/mods.json";
+    private static final String FILE_NAME = "config/temp/mods.json";
+    private static final String FILE_URL = "https://raw.githubusercontent.com/accessible-minecraft/mod-manager-configs/main/mods.json";
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-    public void getSupportedModsList() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
-                FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-            }
-        } catch (IOException e) {
-            // handle exception
-        }
+    public static List<String> getSupportedModsList() {
         try {
-            BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
+            // Store the file temporarily
+            File temp = new File(FILE_NAME);
+            FileUtils.copyURLToFile(new URL(FILE_URL), temp);
+
+            // Get json data
+            JsonNode root = null;
+            mapper.enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
+            root = mapper.readTree(temp);
+
+            // Get supported mod names
+            Iterator<String> mods = root.fieldNames();
+            List<String> modsList = new ArrayList<>();
+
+            // Save mod names in modsList and return it
+            while (mods.hasNext()) {
+                modsList.add(mods.next());
+            }
+
+            // Remove the temporary file
+            temp.delete();
+
+            return modsList;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
---
+++ mod-manager/src/test/java/com/shoaibkhan/modmanager/mods/GetSupportedModsListTest.java
@@ -0,0 +1,10 @@
+package com.shoaibkhan.modmanager.mods;
+
+import org.junit.Test;
+
+public class GetSupportedModsListTest {
+    @Test
+    public void testGetSupportedModsList() {
+        System.out.println(GetSupportedModsList.getSupportedModsList());
+    }
+}
