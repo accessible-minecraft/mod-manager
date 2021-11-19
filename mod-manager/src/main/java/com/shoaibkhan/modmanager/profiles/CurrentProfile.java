682125fbba87d3aba60660d753e643e4dc8f50fa
Shoaib Khan <shoaib.khan20@outlook.com>
2021-11-19 16:36:22 +0530

    Made detecting minecraft version automatic via options.txt file.

--- mod-manager/src/main/java/com/shoaibkhan/modmanager/profiles/CurrentProfile.java
+++ mod-manager/src/main/java/com/shoaibkhan/modmanager/profiles/CurrentProfile.java
@@ -1,69 +1,108 @@
 package com.shoaibkhan.modmanager.profiles;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Paths;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.shoaibkhan.modmanager.configs.Config;
 
 public class CurrentProfile {
 
-    public static String getCurrentProfileName() {
-        try {
-            JsonNode profiles = Config.getData().path("profiles");
+	public static String getCurrentProfileName() {
+		try {
+			JsonNode profiles = Config.getData().path("profiles");
 
-            if (!profiles.isMissingNode()) {
-                String current = profiles.get("current").asText();
-                String name = profiles.path(current).get("name").asText();
-                return name;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			if (!profiles.isMissingNode()) {
+				// Get name from the config.json
+				String current = profiles.get("current").asText();
+				String name = profiles.path(current).get("name").asText();
 
-        // Currupted json file | Reset the json to default
-        Config.setDataToDefault();
-        return "default";
-    }
+				// Add version to name
+				name += " (" + getCurrentProfileVersion() + ")";
 
-    public static String getCurrentProfileDirectory() {
-        try {
-            JsonNode profiles = Config.getData().path("profiles");
+				return name;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            if (!profiles.isMissingNode()) {
-                String current = profiles.get("current").asText();
-                String directory = profiles.path(current).get("location").asText();
-                if (!directory.equalsIgnoreCase("0")) {
-                    if (!utils.checkValidity(directory)) {
-                        // Invalid Directory
-                        RemoveCurrentProfile.removeCurrentProfile();
-                        getCurrentProfileDirectory();
-                    }
-                }
-                return directory;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		// Currupted json file | Reset the json to default
+		Config.setDataToDefault();
+		return "default";
+	}
 
-        // Currupted json file | Reset the json to default
-        Config.setDataToDefault();
-        return utils.getMinecraftDirectory();
-    }
+	public static String getCurrentProfileDirectory() {
+		try {
+			JsonNode profiles = Config.getData().path("profiles");
 
-    public static double getCurrentProfileVersion(){
-        try {
-            JsonNode profiles = Config.getData().path("profiles");
+			if (!profiles.isMissingNode()) {
+				String current = profiles.get("current").asText();
+				String directory = profiles.path(current).get("location").asText();
+				if (!directory.equalsIgnoreCase("0")) {
+					if (!utils.checkValidity(directory)) {
+						// Invalid Directory
+						RemoveCurrentProfile.removeCurrentProfile();
+						getCurrentProfileDirectory();
+					}
+				}
+				return directory;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            if (!profiles.isMissingNode()) {
-                String current = profiles.get("current").asText();
-                double version = profiles.path(current).get("version").asDouble();
-                return version;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		// Currupted json file | Reset the json to default
+		Config.setDataToDefault();
+		return utils.getMinecraftDirectory();
+	}
 
-        // Currupted json file | Reset the json to default
-        Config.setDataToDefault();
-        return 1.17;
-    }
+	public static double getCurrentProfileVersion() {
+		try (BufferedReader optionsFileReader = new BufferedReader(
+				new FileReader(Paths.get(getCurrentProfileDirectory(), "options.txt").toFile()))) {
+			String lineText = null;
+			while ((lineText = optionsFileReader.readLine()) != null) {
+				// Get key
+				String key = lineText.substring(0, lineText.indexOf(":"));
+
+				// Check if current line is the versions line
+				if (key.equalsIgnoreCase("version")) {
+					// Get value
+					String stringVal = lineText.substring(lineText.indexOf(":") + 1);
+
+					int dataVersion = 2730;
+
+					// Convert value to double
+					try {
+						dataVersion = Integer.parseInt(stringVal);
+					} catch (Exception e) {
+						// Corrupted options.txt file, don't change value's value
+						e.printStackTrace();
+					}
+
+					// Convert and return the minecraft version
+					return dataVersionToMinecraftVersion(dataVersion);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return 1.17;
+	}
+
+	private static double dataVersionToMinecraftVersion(int dataVersion) {
+
+		// For 1.17.x
+		if (dataVersion == 2730 || dataVersion == 2724)
+			return 1.17;
+
+		// For 1.16.x
+		if (dataVersion == 2586 || dataVersion == 2584 || dataVersion == 2580 || dataVersion == 2578
+				|| dataVersion == 2567 || dataVersion == 2566)
+			return 1.16;
+
+		return 0;
+	}
 
 }
