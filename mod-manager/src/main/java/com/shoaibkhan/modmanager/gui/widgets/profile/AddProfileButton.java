ac9759985477df5629cbdc01aa83ea7af31336cb
Shoaib Khan <shoaib.khan20@outlook.com>
2021-11-19 16:41:05 +0530

    Removed entering of profile's version when adding a profile

--- mod-manager/src/main/java/com/shoaibkhan/modmanager/gui/widgets/profile/AddProfileButton.java
+++ mod-manager/src/main/java/com/shoaibkhan/modmanager/gui/widgets/profile/AddProfileButton.java
@@ -9,36 +9,23 @@ import com.shoaibkhan.modmanager.profiles.utils;
 import com.shoaibkhan.modmanager.utils.ActionResult;
 
 public class AddProfileButton extends BaseButton {
-    public AddProfileButton(String text) {
-        super(text);
-        this.setPreferredSize(new DimensionUIResource(150, 50));
+	public AddProfileButton(String text) {
+		super(text);
+		this.setPreferredSize(new DimensionUIResource(150, 50));
 
-        this.addActionListener(e -> {
-            String name = JOptionPane.showInputDialog("Enter name of the profile", "Enter profile name");
-            if (name == null)
-                return;
-            double version = 0.0;
-            try {
-                version = Double.parseDouble(JOptionPane.showInputDialog("Enter minecraft version of the profile", "Enter minecraft version"));
-                if (!(version == 1.16 || version == 1.17)) {
-                    JOptionPane.showMessageDialog(this,
-                            "Please enter correct version(only 1.16 and 1.17 are supported).");
-                    return;
-                }
+		this.addActionListener(e -> {
+			String name = JOptionPane.showInputDialog("Enter name of the profile", "Enter profile name");
+			if (name == null)
+				return;
 
-            } catch (Exception exception) {
-                exception.printStackTrace();
-                JOptionPane.showMessageDialog(this, "Please enter correct version(only 1.16 and 1.17 are supported).");
-                return;
-            }
-            String directory = JOptionPane.showInputDialog("Enter directory path", utils.getMinecraftDirectory());
-            if (directory == null)
-                return;
+			String directory = JOptionPane.showInputDialog("Enter directory path", utils.getMinecraftDirectory());
+			if (directory == null)
+				return;
 
-            ActionResult response = AddNewProfile.addNewProfile(name, directory, version);
-            if (response != ActionResult.PASS) {
-                JOptionPane.showMessageDialog(this, response.getDescription());
-            }
-        });
-    }
+			ActionResult response = AddNewProfile.addNewProfile(name, directory);
+			if (response != ActionResult.PASS) {
+				JOptionPane.showMessageDialog(this, response.getDescription());
+			}
+		});
+	}
 }
